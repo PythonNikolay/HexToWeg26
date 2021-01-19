9dbf46b99da6f8cd3967e4ffc1ecbd5c0a9eb468
PythonNikolay <75018332+PythonNikolay@users.noreply.github.com>
2021-01-19 09:19:43 +0300

    multiline input
    version 1.1

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 # Example:
-# card_uid B438F525 = F538B4 (321)
+# Card 0xB438F525 - input without "0x"
+# card_uid B438F525 = F5 38B4 (3,2,1 read byte)
 # F5 to DEC = 245
 # 38B4 to DEC = 14516
-# wiegand = 245,14516
-# 4 byte and 7 byte cards
+# wiegand26 = 245,14516
 
-def uid_to_weg26 (card_uid):
-    card_uid = card_uid
-    uid_to_321 = card_uid[4:6] + card_uid[2:4] + card_uid[0:2]
-    hex_to_dec_1 = int(card_uid[4:6], 16) # Hex to dec 1 element
-    hex_to_dec_2_3 = int(card_uid[2:4] + card_uid[0:2], 16) # Hex to dec 2 and 3 element
-    wiegand26 = (str(hex_to_dec_1).rjust(3, str(0)) + "," + str(hex_to_dec_2_3).rjust(5, str(0)))
-    return print(wiegand26)
+def uid_to_weg26(card_uid):
+	
+	uid_to_321 = card_uid[4:6] + card_uid[2:4] + card_uid[0:2]
+	hex_to_dec_1 = int(card_uid[4:6], 16) # Hex to dec 1 byte
+	hex_to_dec_2_3 = int(card_uid[2:4] + card_uid[0:2], 16) # Hex to dec 2 and 3 byte
+	wiegand26 = (str(hex_to_dec_1).rjust(3, str(0)) + "," + str(hex_to_dec_2_3).rjust(5, str(0)))
+	
+	return print (wiegand26)
 
-uid_to_weg26("B438F525")
-uid_to_weg26("1D115631000001")
+print("Enter/Paste your UIDs. Ctrl-D or Ctrl-Z ( windows ) to convert it.")
+uids = []
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    uids.append(line)
+while("" in uids): 
+    uids.remove("") #remove empty elements
+
+print("---Wiegand26---")
+for x in uids:
+	uid_to_weg26(x)
+
